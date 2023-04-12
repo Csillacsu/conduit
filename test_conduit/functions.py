f246b0ac7c07aabd2c28696b4a6745c9b1ec2e2b
Lengyel Csilla <lengyel.csilla.90@gmail.com>
2023-04-12 21:48:18 +0200

    sign in commit 2

--- test_conduit/functions.py
+++ test_conduit/functions.py
@@ -9,24 +9,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 URL = 'http://localhost:1667'
 
+
 def sign_in(browser):
     browser.find_element(By.XPATH, "//a[@href='#/login']").click()
-    element = WebDriverWait(
-        browser, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'Sign in')]"))
-    )
+    element = WebDriverWait(browser, 20).until(
+        EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'Sign in')]")))
     browser.find_element(By.XPATH, "//input[@placeholder='Email']").send_keys("avokado02@blabla.com")
     browser.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("Avokado02")
     browser.find_element(By.XPATH, "//button[contains(text(),'Sign in')]").click()
-    element = WebDriverWait(
-        browser, 20).until(
-        EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'Your Feed')]"))
-    )
+    element = WebDriverWait(browser, 20).until(
+        EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'Your Feed')]")))
 
 
 def wait_for_element(browser, value):
-    element = WebDriverWait(
-            browser, 5).until(
-            EC.visibility_of_element_located((By.XPATH, value))
-        )
+    element = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, value)))
     return element
